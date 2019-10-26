211cf37bb7b3ea3da220275211a9433f48910ce8
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-10-26 16:37:36 +0300

    DifferentialInterpolation: memory usage reduced (2 times less).

--- differentialinterpolation.c
+++ differentialinterpolation.c
@@ -6,7 +6,7 @@
 void DifferentialInterpolation (double (*F) (double), double min, double max, int points, double **coeffs)
 {
     double *x = malloc (sizeof (double) * points);
-    double *y = malloc (sizeof (double) * points * points);
+    double *y = malloc (sizeof (double) * (points + 1) * points / 2);
     *coeffs = malloc (sizeof (double) * points);
 
     double step = (max - min) / (points - 1);
@@ -18,19 +18,22 @@ void DifferentialInterpolation (double (*F) (double), double min, double max, in
 
     for (int col = 1; col < points; ++col)
     {
+        int sourceCol = col - 1;
+        int sourceColOffset = (points * 2 - sourceCol + 1) * sourceCol / 2;
+        int targetColOffset = (points * 2 - col + 1) * col / 2;
+
         for (int row = 0; row < points - col; ++row)
         {
             int i1 = row;
             int i2 = i1 + col;
-            int sourceCol = col - 1;
-            y[col * points + row] =
-                (y[sourceCol * points + i2 - sourceCol] - y[sourceCol * points + i1]) / (x[i2] -x[i1]);
+            y[targetColOffset + row] = (y[sourceColOffset + i2 - sourceCol] - y[sourceColOffset + i1]) / (x[i2] - x[i1]);
         }
     }
 
-    for (int index = 0; index < points; ++index)
+
+    for (int index = 0, tableIndex = 0; index < points; tableIndex += points - index, ++index)
     {
-        (*coeffs)[index] = y[index * points];
+        (*coeffs)[index] = y[tableIndex];
     }
 
     free (x);
